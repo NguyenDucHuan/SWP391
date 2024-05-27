192918d5b108bff14e17502b754806bbd2888ac6
nguyenduchuan <nguyenduchuan14012003@gmail.com>
2024-05-28 05:21:02 +0700

    Diamonds Page(trang Shop) + filter cho trang tổng (chưa làm phân trang) (chưa làm hiện chi tiết sản phẩm)

---
+++ ProjectDiamondShop/Models/Diamond.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectDiamondShop.Models
+{
+    public class Diamond
+    {
+        public int diamondID { get; set; }
+        public string diamondName { get; set; }
+        public decimal diamondPrice { get; set; }
+        public string diamondDescription { get; set; }
+        public float caratWeight { get; set; }
+        public string clarityID { get; set; }
+        public string cutID { get; set; }
+        public string colorID { get; set; }
+        public string shapeID { get; set; }
+        public string diamondImagePath { get; set; }
+        public bool status { get; set; }
+
+    }
+}
---
+++ ProjectDiamondShop/Scripts/ProductJs/SliderMaxMin.js
@@ -0,0 +1,91 @@
+﻿function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+    if (from > to) {
+        fromSlider.value = to;
+        fromInput.value = to;
+    } else {
+        fromSlider.value = from;
+    }
+}
+
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+    setToggleAccessible(toInput);
+    if (from <= to) {
+        toSlider.value = to;
+        toInput.value = to;
+    } else {
+        toInput.value = from;
+    }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+    if (from > to) {
+        fromSlider.value = to;
+        fromInput.value = to;
+    } else {
+        fromInput.value = from;
+    }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+    setToggleAccessible(toSlider);
+    if (from <= to) {
+        toSlider.value = to;
+        toInput.value = to;
+    } else {
+        toInput.value = from;
+        toSlider.value = from;
+    }
+}
+
+function getParsed(currentFrom, currentTo) {
+    const from = parseFloat(currentFrom.value);
+    const to = parseFloat(currentTo.value);
+    return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+    const rangeDistance = to.max - to.min;
+    const fromPosition = (from.value - to.min) / rangeDistance * 100;
+    const toPosition = (to.value - to.min) / rangeDistance * 100;
+    controlSlider.style.background = `linear-gradient(
+      to right,
+      ${sliderColor} 0%,
+      ${sliderColor} ${fromPosition}%,
+      ${rangeColor} ${fromPosition}%,
+      ${rangeColor} ${toPosition}%, 
+      ${sliderColor} ${toPosition}%, 
+      ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+    const toSlider = document.querySelector('#toSlider');
+    if (Number(currentTarget.value) <= 0) {
+        toSlider.style.zIndex = 2;
+    } else {
+        toSlider.style.zIndex = 0;
+    }
+}
+
+function attachControlHandlers(divId) {
+    const div = document.getElementById(divId);
+    const fromSlider = div.querySelector('#fromSlider');
+    const toSlider = div.querySelector('#toSlider');
+    const fromInput = div.querySelector('#fromInput');
+    const toInput = div.querySelector('#toInput');
+
+    fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+    toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+    fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+    toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+}
+
+attachControlHandlers('CaratDiv');
+attachControlHandlers('PriceDiv');
